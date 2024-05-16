c80ee6b5a5e2fd4bc33d1c94fca1e0504e8c7e65
iwonaive <szymaniak.iwona11@gmail.com>
2024-05-16 20:52:58 +0200

    added service and json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { DataService } from './services/data.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  providers: [DataService],
 })
 export class AppComponent {
   title = 'appointy';
+
+  constructor(private dataService: DataService) {}
 }
